926b3dc4b54303409806d1818e1fbdbe3a13aa52
John van Zalk <jvanzalk@SEPA.local>
2020-04-04 15:47:06 -0400

    Markup Create Leaflet

--- Create_Leaflet.R
+++ Create_Leaflet.R
@@ -1,25 +1,23 @@
-#!/usr/bin/Rscript
+#Load packages
+library(leaflet) #Mapping
+library(sp) #Combining polygon data with a simple df
+library(dplyr) #Data manipulation
+library(rgdal) #Loading spatial data
+library(rgeos) #Simplfying polygons
+library(htmlwidgets) #Turning a map into an html file
+library(aws.s3) #Writing objects to AWS S3
 
-#load packages
-library(leaflet)
-library(sp)
-library(dplyr)
-library(rgdal)
-library(rgeos)
-library(sf)
-library(maptools)
-library(htmlwidgets)
-library(aws.s3)
-
-#read shapefile
+#Read shapefiles produced in Filter_Utilities.R
 utility_shapes <- readOGR(dsn = "Carbon_Reduction_Tracker/filtered_territories", layer = "filtered_territories")
-names(utility_shapes@data) <-c("ID","Name","Has_Carbon_Reduction_Tracker_Goal__c","SEPA_Sub_Organization_Type__c","BillingState", "Has_Emission_Reduction_Goal__c", "Emission_Reduction_Target_Year__c", "Emission_Reduction_Goal__c", "Has_Ren__c", "Renewable_Energy_Target_Year__c","Renewable_Energy_Goal__c")
+names(utility_shapes@data) <-c("ID","Name","SEPA_Sub_Organization_Type__c","BillingState", "Has_Emission_Reduction_Goal__c", "Emission_Reduction_Target_Year__c", "Emission_Reduction_Goal__c")
 data <-utility_shapes@data
 
+#Simply polygons to improve performance on website
 utility_shapes_simple<-gSimplify(utility_shapes, tol = 0.05, topologyPreserve = TRUE)
+#Reattach data to polygons
 utility_shapes<- sp::SpatialPolygonsDataFrame(utility_shapes_simple, data)
 
-emission_shapes <-utility_shapes[utility_shapes@data$Has_Emission_Reduction_Goal__c %in% "true",]
+#Make utility names all uppercase
 emission_shapes@data$Name <- toupper(emission_shapes@data$Name)
 
 #Create csv
@@ -27,12 +25,13 @@ utility_data <- emission_shapes@data[-c(1,3,6,9:11)]
 names(utility_data) <-c("Name","Utility Type","State", "Emission Reduction Target Year","Emission Reduction Goal")
 write.csv(utility_data,"Carbon_Reduction_Tracker/table.csv", row.names = FALSE)
 
+#Authenticate to AWS
 Sys.setenv("AWS_ACCESS_KEY_ID" = "","AWS_SECRET_ACCESS_KEY" = "")
 
-
+#Upload csv
 put_object(file = "Carbon_Reduction_Tracker/table.csv", object = "tracker_data.csv", bucket = "sepa-utility-crt")
 
-#store leaflet in utility_map object
+#Store leaflet in utility_map object
 utility_map <- leaflet() %>%
   fitBounds(-124, 49, -65, 25) %>%
   addTiles() %>%
@@ -45,9 +44,10 @@ utility_map <- leaflet() %>%
               popup = paste("<b style='color: #00a0df;font-family: Arial;font-size:14px'>",emission_shapes@data$Name,"</b>","<br>",
                             "<b style='color: #223a6f;font-family: Arial'>Emission Reduction Goal:</b>","<span style='color: #223a6f;font-family: Arial'>",emission_shapes@data$Emission_Reduction_Goal__c,"</span>"))
 
-#save map as html to folder
+#Save map as html
 htmlwidgets::saveWidget(utility_map,"/home/rstudio/Carbon_Reduction_Tracker/map.html")
 
+#Upload html
 put_object(file = "/home/rstudio/Carbon_Reduction_Tracker/map.html", object = "map.html", bucket = "sepa-utility-crt")
 
 
