abfbe720ea96867f9159a14814a64f59c20353ac
John van Zalk <jvanzalk@SEPA.local>
2020-04-04 14:05:42 -0400

    Remove renewable goals

--- Filter_Utilities.R
+++ Filter_Utilities.R
@@ -1,39 +1,22 @@
-#!/usr/bin/Rscript
-
-#Using crontab
-#To edit or create new schedule past crontab -e in Terminal
-#Press i to enter insert mode
-#MIN HR DAY MON DOY CMD
-#Time is in UTC! (UTC is 4 hours later than EDT)
-#Insert /usr/bin/Rscript at top of script or in cron job line
-#Press CTRL+C to exit insert mode
-#Save and quit by typing :wq
-#Make R script executable with chmod +x /home/user/Location/Of/Script 
-
 #Load required packages
-library(leaflet)
-library(rgdal)
-library(rgeos)
-library(sf)
-library(maptools)
-library(RForcecom)
+library(rgdal)#Loading and writing spatial data
+library(RForcecom)#Querying Saleforce
 
-#Login to Salesforce
-username <- ""
-password <- ""
-loginURL <- ""
-apiVersion <- ""
+#Authenticate to Salesforce
 session <- rforcecom.login(username, password, loginURL, apiVersion)
 
-#Load the shapefiles to Spatial Point Data Frame (SPDF) by referncing the basename (layer)
+#Load the shapefiles from Initial_Matching.R to spdf by referncing the basename (layer)
 utility_shapes <- readOGR(dsn="/home/rstudio/Carbon_Reduction_Tracker/all_territories", layer = "all_territories")
 
-AccountQuery<- "SELECT Account_18_Digit_ID__c,Has_Carbon_Reduction_Tracker_Goal__c,SEPA_Sub_Organization_Type__c,BillingState,Has_Emission_Reduction_Goal__c,Emission_Reduction_Target_Year__c,Emission_Reduction_Goal__c,Has_Ren__c,Renewable_Energy_Target_Year__c,Renewable_Energy_Goal__c FROM Account WHERE Has_Carbon_Reduction_Tracker_Goal__c = TRUE"
+#Retrieve goals from utility accounts in Salesforce
+AccountQuery<- "SELECT Account_18_Digit_ID__c,Has_Carbon_Reduction_Tracker_Goal__c,SEPA_Sub_Organization_Type__c,BillingState,Has_Emission_Reduction_Goal__c,Emission_Reduction_Target_Year__c,Emission_Reduction_Goal__c FROM Account WHERE Has_Carbon_Reduction_Tracker_Goal__c = TRUE"
 goals<-rforcecom.query(session, AccountQuery)
-goals <- goals[, c("Account_18_Digit_ID__c","Has_Carbon_Reduction_Tracker_Goal__c", "SEPA_Sub_Organization_Type__c","BillingState", "Has_Emission_Reduction_Goal__c", "Emission_Reduction_Target_Year__c", "Emission_Reduction_Goal__c", "Has_Ren__c", "Renewable_Energy_Target_Year__c","Renewable_Energy_Goal__c")]
+goals <- goals[, c("Account_18_Digit_ID__c","Has_Carbon_Reduction_Tracker_Goal__c", "SEPA_Sub_Organization_Type__c","BillingState", "Has_Emission_Reduction_Goal__c", "Emission_Reduction_Target_Year__c", "Emission_Reduction_Goal__c")]
 
+#Merge utiltiy territory polygons with goals
 utility_shapes <-merge(x = utility_shapes, y = goals, by.x = "ID", by.y= "Account_18_Digit_ID__c", all.x= TRUE)
 
+#Remove polygons that don't have goals
 utility_shapes <-utility_shapes[!is.na(utility_shapes@data$Has_Carbon_Reduction_Tracker_Goal__c), ]
 
 #delete old files
@@ -42,6 +25,7 @@ file.remove("Carbon_Reduction_Tracker/filtered_territories/filtered_territories.
 file.remove("Carbon_Reduction_Tracker/filtered_territories/filtered_territories.shp")
 file.remove("Carbon_Reduction_Tracker/filtered_territories/filtered_territories.shx")
 
+#Save
 writeOGR(utility_shapes, dsn="Carbon_Reduction_Tracker/filtered_territories", layer= "filtered_territories", driver="ESRI Shapefile")
 
 #to see shapes run: plot(utility_shapes, lwd=0.3)
